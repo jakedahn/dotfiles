01951d8a01aa531bd6012824cba7a22eee017b84
jakedahn <jake@markupisart.com>
2012-11-27 14:48:00 -0800

    more improvements

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ sudo apt-get install ruby1.9.3 rake git zsh -y
 
 # install oh my zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
 # install janus
 curl -Lo- https://bit.ly/janus-bootstrap | bash
@@ -15,10 +14,8 @@ git clone https://github.com/scrooloose/syntastic.git ~/.janus/syntastic
 git clone https://github.com/Lokaltog/vim-powerline.git ~/.janus/vim-powerline
 
 # install dotfiles
-git clone https://jakedahn@github.com/jakedahn/dotfiles.git
-cp -r ./dotfiles/.* ~/
+cp -r ./.* ~/
 
 # echo at end
-
 echo "Everything has been installed.\n"
 echo "## NOTE: You should now run: chsh -s /bin/zsh"
