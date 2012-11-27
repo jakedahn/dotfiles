aac80ce47afe78889fc2e27cb8e7c227d3e7dd17
jakedahn <jake@markupisart.com>
2012-11-27 14:54:51 -0800

    adding cleaner

---
+++ clean.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+rm -rf ~/.gitconfig
+rm -rf ~/.gitignore
+rm -rf ~/.git
+rm -rf ~/.zshrc
+rm -rf ~/.oh-my-zsh
+rm -rf ~/.screenrc
+rm -rf ~/.vimrc.after
+rm -rf ~/.vimrc
+rm -rf ~/.vim
+rm -rf ~/.janus
+
+# echo at end
+echo "Everything has been removed.\n"
+
