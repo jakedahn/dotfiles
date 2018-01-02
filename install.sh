cc63a7e65a92a1badf3fa4338dc302bcf81abbbe
jakedahn <jake@planet.com>
2018-01-01 18:18:53 -0700

    Removing old vimrc copies

--- install.sh
+++ install.sh
@@ -11,8 +11,6 @@ cp -r .gitconfig ~/.gitconfig
 cp -r .gitignore ~/.gitignore
 cp -r .zshrc ~/.zshrc
 cp -r .vimrc.after ~/.vimrc.after
-cp -r .vimrc ~/.vimrc
-cp -r .vim ~/.vim
 
 echo "Everything has been installed.\n"
 echo "## NOTE: You should now run: chsh -s /bin/zsh"
