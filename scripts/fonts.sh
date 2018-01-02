fc430aaae1fe849b6ee8a16cf58261a35ab3bcdd
jakedahn <jake@planet.com>
2018-01-01 17:07:41 -0700

    Trying some magical relative path shenanigans

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+CWD=$(cd $(dirname $0) && pwd)
+
 mkdir -p ~/.local/share/fonts
-cp -r ../vendor/fonts/* ~/.local/share/fonts
+cp -r $CWD/../vendor/fonts/* ~/.local/share/fonts
